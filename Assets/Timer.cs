5a12a4f6ec86e68e8aa8b27f0ab58e2b29fa049b
taeolim <taeolim@gmail.com>
2020-05-24 17:57:52 +0900

    1

---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{ 
+
+    public Text timer;
+    public float time;
+    private void Awake()
+    {
+    }
+
+    private void Update() { 
+        if (time > 0)
+            time -= Time.deltaTime;
+
+        if (time < 0)
+         Destroy(timer);
+   
+
+
+        timer.text = Mathf.Ceil(time).ToString();
+
+
+    }
+
+
+
+    }
